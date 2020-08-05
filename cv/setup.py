87540b4ff0fd43d7a8718ab61338d584e7cce20d
k <k@kdoran.com>
2020-08-05 16:28:21 +0900

    Setting up Python package.

---
+++ cv/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name='project1',
+    packages=['project1'],
+    # This sets the directory, src, to be the root package (denoted by '').                    
+    package_dir={'', 'src'}
+)
